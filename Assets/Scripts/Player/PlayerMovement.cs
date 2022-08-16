ed7f0e7a9bc6545b3770f4315e928277d9a6c8a8
Sreyash Rajalingam Ananth <rasreyash@gmail.com>
2022-08-16 21:08:45 +0100

    Player animations and basic input script setup

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private Animator _animator;
+
+    private float _walkSpeed = 0f;
+    public float WalkSpeed
+    {
+        get { return _walkSpeed;}
+        //set { value = this; }
+    }
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+    }
+}
